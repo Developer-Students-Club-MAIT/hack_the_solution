999c998100794a9ff671619365f8afaed1e0c6db
roopamgarg <roopamg777@gmail.com>
2021-01-20 13:20:24 +0530

    changes

--- src/js/track-modal.js
+++ src/js/track-modal.js
@@ -3,68 +3,36 @@
 
   var tracks = [
     {
-      id: "ai",
-      title: "Artificial Intelligence",
+      id: "Good Health & Wellbeing",
+      title: "Good Health & Wellbeing",
       body:
-        "From bots, to personal assistants, and shopping automation, what the future holds for artificial intelligence can be described for sure with at least one word; Exciting. Our machines and devices are getting faster, more efficient and slowly learning to do things on their own.",
-      img: "img/track-ai.png"
+        "You can start by promoting and protecting your own health and the health of those around you, by making well-informed choices, practicing safe sex, and vaccinating your children. You can raise awareness in your community about the importance of good health, healthy lifestyles, as well as peoples’ rights to quality health care services -especially for the most vulnerable, such as women and children. You can also hold your government, local leaders, and other decision makers accountable to their commitments to improve people’s access to health and health care.",
     },
     {
-      id: "blockchain",
-      title: "Blockchain",
-      body:
-        'Today\'s most exciting use cases for blockchain technology extend far beyond payments and digital currency. With the advent of "smart contracts" and turing-complete frameworks, blockchain technology has evolved far beyond just bitcoins. It is time to join the Web 3.0 revolution!',
-      img: "img/track-blockchain.png"
+      id: "Quality Education",
+      title: "Quality Education",
+      body: "Ask our governments to place education as a priority in both policy and practice. Lobby our governments to make firm commitments to provide free primary school education to all, including vulnerable or marginalized groups."
     },
     {
-      id: "iot",
-      title: "Internet of Things",
-      body:
-        "A world of devices connected by internet opens up opportunities for us that were previously unthinkable. Innovative apps can be built that can allow devices to share data and take intelligent decisions.",
-      img: "img/track-iot.png"
+      id: "Sustainable Cities",
+      title: "Sustainable Cities",
+      body: "Take an active interest in the governance and management of your city. Advocate for the kind of city you believe you need. Develop a vision for your building, street, and neighbourhood, and act on that vision. Are there enough jobs? Can your children walk to school safely? Can you walk with your family at night? How far is the nearest public transport? What’s the air quality like? What are your shared public spaces like? The better the conditions you create in your community, the greater the effect on quality of life."
     },
     {
-      id: "algorithm",
-      title: "Algorithm",
-      body:
-        "Ever wondered why is there a price hike while we hunt for the same flight over and over? Algorithms do it for the Airlines. This concept of Algorithms has existed for centuries; from increasing your flight fares to making your work easier online, algorithms are an integral part of how computers process data. With this track, you challenge world's best programmers and put your coding skills at their best!",
-      img: "img/track-algorithm.png"
+      id: "Decent Work & Economic Growth",
+      title: "Decent Work & Economic Growth",
+      body:"Providing youth with the best opportunity to transition to a decent job calls for investing in education and training of the highest possible quality, providing youth with skills that match labour market demands, giving them access to social protection and basic services regardless of their contract type, as well as levelling the playing field. "
     },
     {
-      id: "fintech",
-      title: "FinTech",
-      body:
-        "The financial services industry is facing a wave of digital disruption that is starting to reshape the sector. This industry is currently the second-biggest target for disruption, after health care.",
-      img: "img/track-fintech.png"
+      id: "Industry, Innovation, & Infrastructure",
+      title: "Industry, Innovation, & Infrastructure",
+      body:"Establish standards and promote regulations that ensure company projects and initiatives are sustainably managed. Collaborate with NGOs and the public sector to help promote sustainable growth within developing countries. Think about how industry impacts on your life and well-being and use social media to push for policymakers to prioritize the SDGs.",
     },
     {
-      id: "future-mobility",
-      title: "Future Mobility",
-      body:
-        "From self driving cars to smart cities software and sensor technology is changing the way we move around. Hate traffic? We too. Hate pollution? Same. Hate finding a parking spot? Yep. Join the Future Mobility track and solve these problems.",
-      img: "img/track-future-mobility.png"
+      id: "Reduced Inequalities",
+      title: "Reduced Inequalities",
+      body: "Reducing inequality requires transformative change. Greater efforts are needed to eradicate extreme poverty and hunger, and invest more in health, education, social protection, and decent jobs - especially for young people, migrants and refugees and other vulnerable communities. Within countries, it is important to empower and promote inclusive social and economic growth. We can ensure equal opportunity and reduce inequalities of income if we eliminate discriminatory laws, policies, and practices.",
     },
-    {
-      id: "healthtech",
-      title: "HealthTech",
-      body:
-        "None of us need to search far for a friend or family member impacted by sickness or injury. At our most vulnerable moments, we are confronted with a healthcare system woefully incapable of meeting the current demands. We are calling on all hackers and sponsors to innovate for a healthier future.",
-      img: "img/track-healthtech.png"
-    },
-    {
-      id: "ml",
-      title: "Machine Learning",
-      body:
-        "Are you fascinated by how Netflix recommends the movies you’ll like? Have you wondered what is the Google algorithm that shows you such accurate search results? Machine Learning is behind these innovations. It represents a key evolution in the fields of computer science, data analysis, software engineering, and artificial intelligence.",
-      img: "img/track-ml.png"
-    },
-    {
-      id: "ar-vr",
-      title: "AR/VR",
-      body:
-        "VR and AR is the extension of our minds, and is meant for those interested in building immersive, engaging experiences for real industry applications. From fashion and sports to data visualisation and engineering to education and healthcare, every sector will be impacted by the rise of VR & AR.",
-      img: "img/track-ar-vr.png"
-    }
   ];
 
   function getNextTrackIndex(currentTrackId) {
